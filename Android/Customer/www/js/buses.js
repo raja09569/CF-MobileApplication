87e1878e72dbc7519b464129e3f9c7b24c5a6888
raj09569 <rajashekar.pandu@gmail.com>
2018-08-12 17:33:18 +0530

    bus list design completed

--- Android/Customer/www/js/buses.js
+++ Android/Customer/www/js/buses.js
@@ -19,10 +19,40 @@ function onResume(){
 	console.log("On Resume called");
 }
 
-/* Home page */
+$(document).on("pageinit", "#bus-search-form", function(event){
+	//console.log(" Bus search form page is opened");
+	$("#btn_fndBus").on("click", function(){
+		/*$("input[type='text']").css('border', 'none');
+		var busFrom = $("input[type='text'][name='bus-from']").val();
+		var busTo = $("input[type='text'][name='bus-to']").val();
+		if(busFrom == ""){
+			$("input[type='text'][name='bus-from']").css('border', '1px solid red');
+			return;
+		}
+		if(busTo == ""){
+			$("input[type='text'][name='bus-to']").css('border', '1px solid red');
+			return;
+		}*/
+		$.mobile.changePage("#page-buses", { transition: "slide", changeHash: false});	
+	});
+});
 
-function selectCity(){
+function selectCity(elem){
+	var elemIs = $(elem).attr('name');
+	//console.log("Element is "+elemIs);
+	window.sessionStorage.setItem('elemIs', elemIs);
 	$.mobile.changePage("#page-cities", { transition: "slideup", changeHash: false});
 }
 
-/*End of Home page */
+function takeCity(elem){
+	var city = $(elem).text().trim();
+	var elemIs = window.sessionStorage.getItem('elemIs');
+	if(elemIs != null){
+		if(elemIs == "bus-from"){
+			$("input[type='text'][name='bus-from']").val(city);		
+		}else{
+			$("input[type='text'][name='bus-to']").val(city);
+		}
+	}
+	$.mobile.changePage("#bus-search-form", { transition: "slidedown", changeHash: false});
+}
